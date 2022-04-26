125a7c5551a4535b89b47ce9270a00feba5786ed
DESKTOP-UQRRQVF\Tran <lytranuit@gmail.com>
2022-04-26 14:34:41 +0700

    update

--- app/Views/frontend/layouts/main.php
+++ app/Views/frontend/layouts/main.php
@@ -28,13 +28,13 @@
     <link rel="stylesheet" id="font-css" href="https://fonts.googleapis.com/css?family=Arimo%3A400%2C400i%2C700&amp;display=swap&amp;ver=5.7.5" type="text/css" media="all">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link href="<?= base_url("/assets/css/bootstrap-grid.css") ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url("/assets/css/bootstrap-grid.css?v=4") ?>" rel="stylesheet" type="text/css">
 
 
     <link href="<?= base_url("/assets/css/menu.css") ?>" rel="stylesheet" type="text/css">
-    <link href="<?= base_url("/assets/css/main.css?v=3") ?>" rel="stylesheet" type="text/css">
-    <link href="<?= base_url("/assets/css/resp.cs") ?>s" rel="stylesheet" type="text/css">
-    <link href="<?= base_url("/assets/css/infoblock.css") ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url("/assets/css/main.css?v=4") ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url("/assets/css/resp.css?v=4") ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url("/assets/css/infoblock.css?v=4") ?>" rel="stylesheet" type="text/css">
     <link href="<?= base_url("/assets/css/infoblockResp.css") ?>" rel="stylesheet" type="text/css">
     <link href="<?= base_url("/assets/css/slider.css?v=2") ?>" rel="stylesheet" type="text/css"><!-- Facebook Pixel Code -->
 
