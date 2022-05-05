f57fdae8962f963651a8c1e854d909b470eb3cf2
DESKTOP-UQRRQVF\Tran <lytranuit@gmail.com>
2022-05-05 16:31:35 +0700

    update

--- app/Views/frontend/product/view.php
+++ app/Views/frontend/product/view.php
@@ -250,8 +250,28 @@
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 3,
+            dots: false,
+            adaptiveHeight: true,
             prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
             nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     })
 </script>
