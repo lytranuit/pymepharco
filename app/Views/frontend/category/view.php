4e73804d3d87b61278654ea1269d8681ffc55889
DESKTOP-UQRRQVF\Tran <lytranuit@gmail.com>
2022-05-06 14:14:07 +0700

    update

--- app/Views/frontend/category/view.php
+++ app/Views/frontend/category/view.php
@@ -6,7 +6,7 @@
 <?= view_cell("\App\Libraries\HeaderWidget::breadcrumb") ?>
 
 
-<div id="pageImage" style="background-image: url(<?= base_url($info->image_url) ?>);">
+<div id="pageImage" style="background-image: url(<?= base_url($category->image_url) ?>);">
 
     <div id="pageImageOverlay">
         <div class="circle circleNo1"></div>
