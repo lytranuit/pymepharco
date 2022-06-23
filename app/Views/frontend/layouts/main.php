fb8d423484a1dd1d5b12a8c552d224db099b1133
DESKTOP-UQRRQVF\Tran <lytranuit@gmail.com>
2022-06-23 14:46:29 +0700

    update

--- app/Views/frontend/layouts/main.php
+++ app/Views/frontend/layouts/main.php
@@ -2,7 +2,7 @@
 <!-- saved from url=(0020)https://hoptacxa.vn/ -->
 <html lang="vi-VN" class="js">
 
-<?php $ver = 3 ?>
+<?php $ver = 4 ?>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
